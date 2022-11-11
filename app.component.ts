2cc7d1c2f4dcecf6d3fa4359c35626cd47157e84
Rajani989 <99346036+Rajani989@users.noreply.github.com>
2022-11-11 12:01:00 +0530

    commit

---
+++ app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Rajani.CV';
+ name='Rajani Kumari';
+  path='../assets/m1.jpg';
+}

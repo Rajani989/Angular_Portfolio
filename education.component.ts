0a01eae131382f54aaaf096eaa45cd61e27f80a3
Rajani989 <99346036+Rajani989@users.noreply.github.com>
2022-11-11 14:42:10 +0530

    commit

---
+++ education.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-education',
+  templateUrl: './education.component.html',
+  styleUrls: ['./education.component.css']
+})
+export class EducationComponent  {
+
+  name='Education';
+}
